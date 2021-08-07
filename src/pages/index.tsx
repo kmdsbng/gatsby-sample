4d7d49a08d7ef597b304bcf31026813eb15eb302
Yoshihiro Kameda <kameda.sbng@gmail.com>
2021-08-07 20:47:40 +0900

    change index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,29 @@
 import React from "react"
-import { PageProps } from "gatsby"
+import { Link, PageProps } from "gatsby"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import { StaticImage } from "gatsby-plugin-image"
 export default function IndexRoute(props: PageProps) {
   return (
-    <>
-      <h1>Path:</h1>
-      <p>{props.path}</p>
-    </>
+
+    <Layout>
+      <Seo title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <StaticImage
+        src="../images/gatsby-astronaut.png"
+        width={300}
+        quality={95}
+        formats={["auto", "webp", "avif"]}
+        alt="A Gatsby astronaut"
+        style={{ marginBottom: `1.45rem` }}
+      />
+      <p>
+        <Link to="/page-2/">Go to page 2</Link> <br />
+        <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+      </p>
+    </Layout>
   )
 }
+
